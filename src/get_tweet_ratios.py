bdfd9b0031b97a21c815343be6e550685cc3e26e
sleepless13 <zmigliorini@gmail.com>
2017-03-08 11:35:33 -0500

    Final version with URL ratio

--- src/get_tweet_ratios.py
+++ src/get_tweet_ratios.py
@@ -14,13 +14,15 @@ dow_ratios = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
 
 
 
-#Takes a user_id and returns a 3-Tuple (A, B, C)
+#Takes a user_id and returns a 4-Tuple (A, B, C, D)
 #A: int
     #How many tweets were iterated through
-#B: dict
+#B: float
+    #Ratio of URLs posted to total tweets
+#C: dict
     #Keys are the sources of all the users tweets
     #Values are the relative frequencies of tweets from each source
-#C: dict
+#D: dict
     #Keys are day of week, 0-6 maps to Sun-Sat
     #Values are relative frequencies of tweets on each day
 def get_tweet_ratios(user_id):
@@ -30,15 +32,22 @@ def get_tweet_ratios(user_id):
 
     total_tweets_recorded = 0
 
+    urls_recorded = 0
+    
     #If this account is protected we cannot see thier tweets and should skip
     #Once further progress is made, this check will likely be done at a higher level,
     #   and the user account will not even make it to this stage
     if not user_data['protected']:
         #Iterate through all (3200 max) tweets. items() can take a lower max to limit
-        for tweet in tweepy.Cursor(api.user_timeline, id=user_id).items():
+        for tweet in tweepy.Cursor(api.user_timeline, id=user_id, tweet_mode='extended').items():
             update_source_ratios(tweet.source)
             update_dow_ratios(tweet.created_at.weekday())
 
+            #If this tweet contained urls, count them - later will use Google or Cymon API
+            #   to check if the urls are threats/malicious
+            if len(tweet.entities['urls']) > 0:
+                urls_recorded += len(tweet.entities['urls'])
+                
             #Summing the total recorded values here, rather than user_data[statuses_count]
             #   in case we want to change items() to some number items(n), to only pull n tweets
             #Not sure how to do this with variable args, as we don't have a constant default for n
@@ -53,14 +62,16 @@ def get_tweet_ratios(user_id):
         for key in dow_ratios:
             flat_val = dow_ratios[key]
             dow_ratios[key] = flat_val/total_tweets_recorded
+
+        #Calculate ratio of total urls posted over total tweets
+        urls_ratio = urls_recorded/total_tweets_recorded
         
-        return total_tweets_recorded, source_ratios, dow_ratios
+        return total_tweets_recorded, urls_ratio, source_ratios, dow_ratios
 
     else:
-        print("protected")
-        return -1, {}, {}
-
-
+        print("Protected: {}".format(user_id))
+        return -1, -1, {}, {}
+    
 
 def update_source_ratios(source):
     if source in source_ratios:
@@ -68,10 +79,11 @@ def update_source_ratios(source):
     else:
         source_ratios[source] = 1
 
+
 def update_dow_ratios(weekday):
     dow_ratios[weekday] += 1
-        
 
+        
 #Example
 
 #Non-protected case
