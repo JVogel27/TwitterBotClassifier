05fce1aa280bd6059c33acbc7e5d4b03430ae8d9
Jesse Vogel <JBV8042@rit.edu>
2017-04-09 16:21:40 -0400

    reorganized runner to prepare for passing input to classifier

--- src/runner.py
+++ src/runner.py
@@ -5,21 +5,28 @@ import numpy as np
 from visualize import visualize_decision_regions
 import keys as key_reader
 import os
+import sys
 
 
 def main():
+
+	twitter_user_name = sys.argv[1]
+
 	# create a list of credential objects
 	keys = key_reader.read_key_file('./keys.txt')
 
 	# the data visualization graph only works for two features, so specify which ones to use
-	first_feature = 2
-	second_feature = 3
+	# first_feature = 2
+	# second_feature = 3
+
+	# TODO add our own data
+	# iris = datasets.load_iris()
+	# X = iris.data[:, [first_feature, second_feature]]
+	# y = iris.target
 
 	# extract our features and class label from the raw data
-	# TODO add our own data
-	iris = datasets.load_iris()
-	X = iris.data[:, [first_feature, second_feature]]
-	y = iris.target
+	X = get_input_data()
+	y = get_class_labels()
 
 	# split data into training and test data
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, train_size=0.66, random_state=42)
@@ -41,14 +48,20 @@ def main():
 	accuracy = rfc.get_classifier_accuracy(predicted_class_labels, y_test)
 	print(accuracy)
 
+	# run user input through classifier
+	input_data = convert_name_to_data(twitter_user_name)
+	result = rfc.predict(input_data)
+	print(result)
+
+
 	# plot the decision boundaries
-	xlabel = iris.feature_names[first_feature]
-	ylabel = iris.feature_names[second_feature]
-	X_combined = np.vstack((X_train, X_test))
-	y_combined = np.hstack((y_train, y_test))
-	test_start = X_train.shape[0]   # use these values to highlight the test data on the graph
-	test_end = X_combined.shape[0]
-	visualize_decision_regions(X_combined, y_combined, xlabel, ylabel, rfc, test_idx=range(test_start, test_end))
+	# xlabel = iris.feature_names[first_feature]
+	# ylabel = iris.feature_names[second_feature]
+	# X_combined = np.vstack((X_train, X_test))
+	# y_combined = np.hstack((y_train, y_test))
+	# test_start = X_train.shape[0]   # use these values to highlight the test data on the graph
+	# test_end = X_combined.shape[0]
+	# visualize_decision_regions(X_combined, y_combined, xlabel, ylabel, rfc, test_idx=range(test_start, test_end))
 
 if __name__ == '__main__':
 	main()
